f916800fc6f76bc7b355acc5c8c60496bee13ab1
cxr020301@darden.com <cxr020301@darden.com>
2023-11-22 19:03:59 +0530

    Task 2

---
+++ student_student/src/main/java/com/example/student/service/StudentServ.java
@@ -0,0 +1,22 @@
+package com.example.student.service;
+ 
+import java.util.List;
+ 
+import org.springframework.stereotype.Service;
+ 
+import com.example.student.model.Student;
+ 
+@Service
+public interface StudentServ {
+ 
+	Student getByName(String name);
+ 
+	void update(Student student);
+ 
+	List<Student> getAllStudents();
+ 
+	void delete(String name);
+ 
+	Student save(Student student);
+ 
+}
